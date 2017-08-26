a4f90a851ce3a3a279b656c050922f82789b032a
Eva Raymond <evathecoderr@outlook.com>
2017-08-26 07:38:50 +0100

    Perfom search via youtube api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import YouTubeSearch from 'youtube-api-search-promise';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = 'AIzaSyCpFxRHZXmSL0vct7PZmZ6GNlWhBSVcT1E';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YouTubeSearch({key: API_KEY, term: 'Fifth Harmony'})
+      .then(result => this.setState({videos: result}))
+      .catch(err => console.error(err, 'cant fetch data'))
+
+  }
+
   render() {
     return (
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
